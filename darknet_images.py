638dfc1a7632d55d4b7d88e27c872b2eb2e6b35d
Denis Reznikov <denis.reznikov@gmail.com>
2020-10-23 14:00:50 +0300

    ADd logging and error

--- darknet_images.py
+++ darknet_images.py
@@ -11,10 +11,12 @@ import darknet
 
 class Yolo_wrapper():
 
-    def __init__(self, config_file="cfg/yolov4-obj.cfg", data_file = "data/obj.data", weights = "custom-yolov4-detector_best.weights"):
+    def __init__(self, logger, config_file="cfg/yolov4-obj.cfg", data_file = "data/obj.data", weights = "custom-yolov4-detector_best.weights"):
+        self.__logger = logger
         self.__config_file = config_file
         self.__data_file = data_file
         self.__weights = weights
+        
     
 
     def __image_detection(self, image, network, class_names, class_colors, thresh):
@@ -49,7 +51,24 @@ class Yolo_wrapper():
 
 
     def classify_photo(self, image_rgb, thresh=0.25, batch_size=1):
+        
+        if not os.path.exists(self.__config_file):
+            self.__logger.error(f'Load file error. Config_file')
+            raise(ValueError("Invalid config path {}".format(os.path.abspath(self.__config_file))))
+        if not os.path.exists(self.__weights):
+            self.__logger.error(f'Load file error. Weights')
+            raise(ValueError("Invalid weight path {}".format(os.path.abspath(self.__weights))))
+        if not os.path.exists(self.__data_file):
+            self.__logger.error(f'Load file error. Data_file')
+            raise(ValueError("Invalid data file path {}".format(os.path.abspath(self.__data_file))))
+
+        if len(set(image_rgb.shape)) > 1:
+            self.__logger.error(f'Images dont have same shape')
+            raise ValueError("Images must have same shape")
+        
         random.seed(3)  
+        prev_time = time.time()
+
         network, class_names, class_colors = darknet.load_network(
             self.__config_file,
             self.__data_file,
@@ -57,10 +76,17 @@ class Yolo_wrapper():
             batch_size=batch_size
         )
 
-        prev_time = time.time()
-        image, detections = self.__image_detection(
-            image_rgb, network, class_names, class_colors, thresh
-            )
+        try:
+            image, detections = self.__image_detection(
+                image_rgb, network, class_names, class_colors, thresh
+                )
+        except Exception:
+            self.__logger.error(f'Error in yolo.In C path')
+
+        fps = int((time.time() - prev_time))
+        self.__logger.info(f'Time for one predict: ' + fps)
+
+
         label = []
         confidence = []
         bbox = []
