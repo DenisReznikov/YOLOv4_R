94e4ac4ae2447e2eedb362c565d3cd32f63b2079
Denis <Denis.reznikov@gmail.com>
2020-10-24 21:48:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,4 +6,13 @@ image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 yolo = Yolo_wrapper(logger=logging)
 label, confidence, bbox = (yolo.classify_photo(image_rgb))
+print("test 1")
+print(label, confidence, bbox)
+
+image = cv2.imread('test2.jpg')
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+yolo = Yolo_wrapper(logger=logging)
+label, confidence, bbox = (yolo.classify_photo(image_rgb))
+print("test 2")
 print(label, confidence, bbox)
